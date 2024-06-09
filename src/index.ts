4bc6f3c706bb20affd88ef7c36680a8fc507b311
venu reddy <venugopalreddy9493@gmail.com>
2024-06-09 18:58:41 +0530

    backend changes

--- src/index.ts
+++ src/index.ts
@@ -38,7 +38,7 @@ const server = http.createServer(app)
 
 const io = new Server(server, {
     cors: {
-        origin: ["http://localhost:5173", 'http://192.168.0.175:5173', 'http://192.168.1.37:5173', 'https://whatsapp-chat-imbu.onrender.com', "https://admin.socket.io"],
+        origin: ["http://localhost:5173", 'https://admin.socket.io', 'https://whatsapp-chat-imbu.onrender.com'],
         credentials: true,
 
     }
