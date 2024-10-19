c83cf8aa49e51986e0e7bb7163d75303087cf47f
venu reddy <venugopalreddy9493@gmail.com>
2024-10-20 00:14:31 +0530

    vercel deploy 1

--- api/index.ts
+++ api/index.ts
@@ -1,11 +1,22 @@
-import { handle } from 'hono/vercel'
-// import { handle } from '@hono/node-server/vercel'
-export const runtime = 'edge'
+// export const runtime = 'edge'
 import app from "../dist/index.js"
 
+// import { Hono } from 'hono'
+import { handle } from '@hono/node-server/vercel'
+import type { PageConfig } from 'next'
 
-export const GET = handle(app.app)
-export const POST = handle(app.app)
-export const PUT = handle(app.app)
-export const DELETE = handle(app.app)
-export const PATCH = handle(app.app)
+export const config: PageConfig = {
+  api: {
+    bodyParser: false,
+  },
+}
+
+// const app = new Hono().basePath('/api')
+
+app.app.get('/hello', (c) => {
+  return c.json({
+    message: 'Hello from Hono!',
+  })
+})
+
+export default handle(app.app)
