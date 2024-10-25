d26f3d1086db4d46ec929695149eb44d26618e60
venu reddy <venugopalreddy9493@gmail.com>
2024-10-25 22:55:55 +0530

    vercel deploy

--- api/index.ts
+++ api/index.ts
@@ -1,15 +1,9 @@
-export const runtime = 'edge'
+// export const runtime = 'edge'
 import app from "../dist/index.js"
 
 // import { Hono } from 'hono'
 import { handle } from '@hono/node-server/vercel'
-import type { PageConfig } from 'next'
 
-export const config: PageConfig = {
-    api: {
-        bodyParser: false,
-    },
-}
 
 
 export default handle(app.app as any)
